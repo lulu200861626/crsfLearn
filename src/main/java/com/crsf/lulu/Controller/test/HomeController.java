8292f1dc30ed7bc5d2626ba932c44acf16bdd02f
lulu05 <lulu05@meituan.com>
2015-08-05 17:43:02 +0800

    update

---
+++ src/main/java/com/crsf/lulu/Controller/test/HomeController.java
@@ -0,0 +1,21 @@
+package com.crsf.lulu.Controller.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lulu on 15/8/5.
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(value = {"/",""})
+    public String home(Model m) {
+        return "index";
+    }
+    @RequestMapping(value = {"home"})
+    public String index(Model m) {
+        return "index";
+    }
+}
